9af2a3ff74a58efe24084ff11a27c40a974ae9f1
Dean <deanpatrickgaffney@gmail.com>
2018-09-29 12:47:05 +0100

    refactored python trelllo script to use configuration file, removed unused files from repo

--- trello.py
+++ trello.py
@@ -4,22 +4,27 @@ import simplejson as json
 def texify(content):
     return re.sub(r'_|\$', '', content)
 
-r = requests.get('https://api.trello.com/1/boards/yxmoMkuv/cards?fields=labels,name&key=54ebccc76a845d4676118df0c6ff9bd6&token=b79225c7073db96bd8275848eb9c4ffdf0912c593c7bc85ad29ddc725a36b735')
-sprint_prefix = '0.0.'
-sprint_dict = {}
-for i in range(1, 7, 1): sprint_dict[sprint_prefix + str(i)] = []
+with open('./conf/conf.json') as conf_file:
+    conf = json.load(conf_file)
+    key = conf["apis"]["trello"]["key"]
+    token = conf["apis"]["trello"]["token"]
+    params = {"key": key, "token": token}
+    r = requests.get('https://api.trello.com/1/boards/yxmoMkuv/cards?fields=labels,name', params=params)
+    sprint_prefix = '0.0.'
+    sprint_dict = {}
+    for i in range(1, 7, 1): sprint_dict[sprint_prefix + str(i)] = []
 
-for resp in r.json():
-    for label in resp['labels']:
-        if(label['name'].startswith(sprint_prefix)):
-            sprint_dict[label['name']].append(resp['name'])
+    for resp in r.json():
+        for label in resp['labels']:
+            if(label['name'].startswith(sprint_prefix)):
+                sprint_dict[label['name']].append(resp['name'])
 
-with open('trello.tex', 'w') as tex_file:
-    tex_file.write('\\begin{document}\n\chapter{Methodology}\n\section{Trello}')
-    for sprint in sprint_dict.keys():
-        tex_file.write('\subsection*{' + sprint + '}\n\\begin{itemize}\n')
-        tex_file.writelines(map(lambda issue: '\item ' + texify(issue) + '\n', sprint_dict[sprint]))
-        tex_file.write('\end{itemize}\n')
-    tex_file.write('\end{document}')
+    with open('trello.tex', 'w') as tex_file:
+        tex_file.write('\\begin{document}\n\chapter{Methodology}\n\section{Trello}')
+        for sprint in sprint_dict.keys():
+            tex_file.write('\subsection*{' + sprint + '}\n\\begin{itemize}\n')
+            tex_file.writelines(map(lambda issue: '\item ' + texify(issue) + '\n', sprint_dict[sprint]))
+            tex_file.write('\end{itemize}\n')
+        tex_file.write('\end{document}')
 
 
